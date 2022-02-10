1ca5e1ca8d43d97a90b93d74158d925b28998114
JouniKiiskila <43674@edu.raseko.fi>
2022-02-10 10:28:17 +0200

    hep

--- test_funktiot_moduli.py
+++ test_funktiot_moduli.py
@@ -1,7 +1,7 @@
-# Ykskkö testi modulille funktiot_moduli.py
+# Yksikkö testit modulille funktiot_moduli.py
 
 import funktiot_moduli
 
 def test_suorakulma():
-    assert funktiot_moduli.suorakulma(3, 4, 5) == 0.0
-    assert round(funktiot_moduli.suorakulma(10,12,14), 4) == -1.6205
+   assert funktiot_moduli.suorakulma(3, 4 ,5) == 0.0
+   assert round(funktiot_moduli.suorakulma(10, 12, 14), 4) == -1.6205
